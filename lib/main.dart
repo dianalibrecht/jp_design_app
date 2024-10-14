a48b1795d16e2af4093595e670a9425d60c069df
Diana <dlibrecht93@gmail.com>
2024-10-14 13:11:59 +0200

    container transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,12 +19,51 @@ class MainApp extends StatelessWidget {
           Image.asset("assets/backgrounds/bg_startscreen.png",
               fit: BoxFit.cover),
           Positioned(
-              top: 200,
-              left: 40,
+              top: 150,
+              left: 12,
               child: Transform.scale(
-                  scale: 1.4,
+                  scale: 1.15,
                   child: Image.asset("assets/grafics/chick_cupcakes.png"))),
-          Image.asset("assets/details/snack.png"),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 40,
+              child: Image.asset("assets/details/snack.png",
+                  fit: BoxFit.fitWidth)),
+          Positioned(
+            bottom: 148,
+            left: 16,
+            right: 16,
+            child: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 40),
+                        const Text("Feeling snackish today?"),
+                        const SizedBox(height: 24),
+                        const Text(
+                            "Explore Angi`s most popular snack selection \n and get instantly happy"),
+                        const SizedBox(height: 40),
+                        ElevatedButton(
+                            style: ButtonStyle(),
+                            onPressed: () {},
+                            child: const Padding(
+                              padding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                              child: Text("Order now",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       )),
     );
