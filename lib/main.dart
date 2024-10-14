e72b383054db8340206b14b14369df3a5359b44f
Diana <dlibrecht93@gmail.com>
2024-10-14 13:59:13 +0200

    main screen finished

--- lib/main.dart
+++ lib/main.dart
@@ -27,36 +27,63 @@ class MainApp extends StatelessWidget {
           Positioned(
               left: 0,
               right: 0,
-              bottom: 40,
+              bottom: 96,
               child: Image.asset("assets/details/snack.png",
                   fit: BoxFit.fitWidth)),
           Positioned(
-            bottom: 148,
+            bottom: 80,
             left: 16,
             right: 16,
             child: ClipRRect(
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
                 child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 0.2),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(16))),
                   child: Center(
                     child: Column(
                       children: [
-                        const SizedBox(height: 40),
-                        const Text("Feeling snackish today?"),
-                        const SizedBox(height: 24),
+                        const SizedBox(height: 32),
                         const Text(
-                            "Explore Angi`s most popular snack selection \n and get instantly happy"),
-                        const SizedBox(height: 40),
-                        ElevatedButton(
-                            style: ButtonStyle(),
-                            onPressed: () {},
+                          "Feeling Snackish today?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 24,
+                              color: Colors.white),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          "Explore Angi`s most popular snack selection \n and get instantly happy",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 32),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                gradient: LinearGradient(
+                                    colors: [
+                                      Color.fromARGB(149, 217, 172, 100),
+                                      Color.fromARGB(233, 201, 75, 157)
+                                    ],
+                                    begin: Alignment.centerRight,
+                                    end: Alignment.centerLeft)),
                             child: const Padding(
-                              padding: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                              child: Text("Order now",
+                              padding: EdgeInsets.fromLTRB(56, 12, 56, 12),
+                              child: Text("Order Now",
                                   style: TextStyle(
                                       color: Colors.white,
+                                      fontSize: 16,
                                       fontWeight: FontWeight.bold)),
-                            ))
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
                       ],
                     ),
                   ),
