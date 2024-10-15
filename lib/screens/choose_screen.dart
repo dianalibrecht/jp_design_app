4bc99240b43d492296510141d7336533cff47031
Diana <dlibrecht93@gmail.com>
2024-10-15 14:23:53 +0200

    added ice container

--- lib/screens/choose_screen.dart
+++ lib/screens/choose_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:jp_design_app/screens/balu_container.dart';
+import 'package:jp_design_app/widgets/mogli_container.dart';
 import 'package:jp_design_app/widgets/categories_row.dart';
 
 class ChooseScreen extends StatelessWidget {
@@ -118,7 +120,87 @@ class ChooseScreen extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           fontSize: 18,
                           color: Colors.white)),
-                  const Row()
+                  const SizedBox(height: 16),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(children: [
+                      const MogliContainer(),
+                      const SizedBox(width: 24),
+                      const BaluContainer(),
+                      const SizedBox(width: 24),
+                      ClipRRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 1000, sigmaY: 1000),
+                          child: Container(
+                            width: 204,
+                            height: 280,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(24),
+                                border:
+                                    Border.all(color: Colors.white, width: 0.2),
+                                gradient: const LinearGradient(
+                                    colors: [
+                                      Color.fromARGB(57, 82, 82, 82),
+                                      Color.fromARGB(132, 134, 90, 255),
+                                      Color.fromARGB(132, 134, 90, 255),
+                                      Color.fromARGB(132, 134, 90, 255),
+                                      Color.fromARGB(155, 105, 28, 177)
+                                    ],
+                                    stops: [
+                                      0.1,
+                                      0.35,
+                                      0.5,
+                                      0.6,
+                                      0.9
+                                    ],
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Transform.scale(
+                                      scale: 1.0,
+                                      child: Image.asset(
+                                          "assets/grafics/ice_cream_stick.png")),
+                                  const Text("Sara`s stick",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12,
+                                          color: Colors.white)),
+                                  const Text("Chocolate ice cream",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w300)),
+                                  const SizedBox(height: 16),
+                                  const Row(
+                                    children: [
+                                      Text(
+                                        "₳8.99",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                      Expanded(child: SizedBox()),
+                                      Icon(Icons.favorite_border_rounded,
+                                          color: Colors.white, size: 12),
+                                      Text("200",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12))
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  )
                 ],
               )),
           Positioned(
---
+++ lib/widgets/mogli_container.dart
@@ -0,0 +1,75 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class MogliContainer extends StatelessWidget {
+  const MogliContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 1000, sigmaY: 1000),
+        child: Container(
+          width: 204,
+          height: 280,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              border: Border.all(color: Colors.white, width: 0.2),
+              gradient: const LinearGradient(colors: [
+                Color.fromARGB(57, 82, 82, 82),
+                Color.fromARGB(132, 134, 90, 255),
+                Color.fromARGB(132, 134, 90, 255),
+                Color.fromARGB(132, 134, 90, 255),
+                Color.fromARGB(155, 105, 28, 177)
+              ], stops: [
+                0.1,
+                0.35,
+                0.5,
+                0.6,
+                0.9
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Transform.scale(
+                    scale: 1.0,
+                    child: Image.asset("assets/grafics/cat_cupcakes.png")),
+                const Text("Mogli`s Cup",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                        color: Colors.white)),
+                const Text("Strawberry ice cream",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 11,
+                        fontWeight: FontWeight.w300)),
+                const SizedBox(height: 16),
+                const Row(
+                  children: [
+                    Text(
+                      "₳8.99",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Expanded(child: SizedBox()),
+                    Icon(Icons.favorite_border_rounded,
+                        color: Colors.white, size: 12),
+                    Text("200",
+                        style: TextStyle(color: Colors.white, fontSize: 12))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
